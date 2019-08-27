0a6266a4a47cdaddaac0230cea37217222c0ac8f
Scott Ammon <scott.o.ammon@gmail.com>
2019-08-26 19:24:46 -0700

    adding basic error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import axios from "axios";
 import dotenv from "dotenv";
+import express from "express";
+import { asyncWrapper } from "./utils/asyncWrapper";
 
 dotenv.config();
 
@@ -7,9 +9,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("*", async (req, res, next) => {
-  res.sendStatus(200);
-});
+app.get("/sample-route", asyncWrapper(async (req, res, next) => {
+  const sampleApi = await axios({
+    method: "GET",
+    url: "http://localhost:3030/test"
+  });
+
+  res.send(sampleApi.data);
+}));
 
 const port = process.env.PORT;
 
