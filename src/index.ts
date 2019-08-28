aa1a2589cd2bc9401e33f764d2287df3c26b2e2f
Scott Ammon <scott.o.ammon@gmail.com>
2019-08-27 17:15:05 -0700

    added logger and updated wrapper/error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,16 @@ import express from "express";
 import { asyncWrapper } from "./utils/asyncWrapper";
 
 dotenv.config();
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const logger = (request: express.Request, response: express.Response, next: any) => {
+  console.log(`${request.method} request was made to ${request.path}`);
+  next();
+};
 
 app.get("/sample-route", asyncWrapper(async (req, res, next) => {
   const sampleApi = await axios({
@@ -18,7 +24,9 @@ app.get("/sample-route", asyncWrapper(async (req, res, next) => {
   res.send(sampleApi.data);
 }));
 
-const port = process.env.PORT;
+app.use((error: any, req: any, res: any, next: any) => {
+  res.status(500).json({ error: "service unavailable" });
+});
 
 app.listen(port || 3000, () => {
   console.log(`Server listening on port ${port}`);
--- src/utils/asyncWrapper.ts
+++ src/utils/asyncWrapper.ts
@@ -1,11 +1,5 @@
 export const asyncWrapper = (fn) =>
   (req: any, res: any, next: any) => {
     Promise.resolve(fn(req, res, next))
-      .catch((error) => {
-        if (error.response.status === 401) {
-          console.log("failed endpoint is: ", error.response.request.path);
-          res.status(401).send({ error: "Unauthorized."});
-        }
-        next(error);
-      });
-};
+      .catch(next);
+  };
